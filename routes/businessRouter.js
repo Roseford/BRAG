a281c56412cddcc932311932298a9c6378b771a0
Roseford <obianujuofodu@gmail.com>
2024-02-08 04:50:45 +0100

    Business

---
+++ routes/businessRouter.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const authController = require('./../controllers/authController');
+const businessController = require('../controllers/businessController')
+const businessMiddleware = require('../middlewares/businessMiddleware')
+const upload = require('./../utils/multer')
+
+const router = express.Router();
+
+router.get('/getAllBusinesses', authController.protect, businessController.getAllBusinesses);
+router.get('/:id', authController.protect, businessController.getSingleBusiness);
+
+router.post('/createBusiness', 
+authController.protect,
+businessMiddleware.attachUserId, 
+upload.fields([
+    { name: 'logo', maxCount: 1 },
+    { name: 'image' }
+]),
+businessMiddleware.uploadAndParseReq,
+businessController.createBusiness);
+
+router.patch('/:id', authController.protect, 
+upload.fields([
+    { name: 'logo', maxCount: 1 },
+    { name: 'image' }
+]),
+businessMiddleware.uploadAndParseReq,
+businessController.updateBusiness);
+
+router.delete('/:id', authController.protect, businessController.deleteBusiness);
+
+
+module.exports = router
